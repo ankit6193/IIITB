311526616eda2eb7ba86dac1b32eee8cd2945bd6
ankit6193 <ankit.naik93@gmail.com>
2016-08-27 01:37:26 +0530

    Pair Addition Check using Hash

---
+++ Semester1/Algorithms/Pair_Addition_Algo2.c
@@ -0,0 +1,20 @@
+/* O(n) Method by using Hash Concept */
+
+#include <stdio.h>
+
+int main(){
+
+	int arr[] = { 10 , 43 , 22 , 45 , 55 , -4 } ; //works for -ve number
+	int hash[1000000] = {0} ;
+	int sum = 6;
+	int size = sizeof(arr) / sizeof(arr[0]);
+	for(int i = 0 ; i < size ; i++){
+		hash[arr[i]] = i ;
+		if(arr[i] < sum){
+			if(hash[sum - arr[i]] != i){
+				printf("Pairs : %d and %d", sum-arr[i] , arr[i]); 
+				break;
+			}
+		} 
+	}
+}

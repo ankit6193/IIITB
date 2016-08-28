21584efc0b0df554991af717fa3fbe8e9a4f75fc
ankit6193 <ankit.naik93@gmail.com>
2016-08-28 17:11:01 +0530

    Pair Addition Check using Hash - UPDATED FOR EASIER UNDERSTANDING

--- Semester1/Algorithms/Pair_Addition_Algo2.c
+++ Semester1/Algorithms/Pair_Addition_Algo2.c
@@ -6,12 +6,16 @@ int main(){
 
 	int arr[] = { 10 , 43 , 22 , 45 , 55 , -4 } ; //works for -ve number
 	int hash[1000000] = {0} ;
-	int sum = 6;
+	int sum = 32;
 	int size = sizeof(arr) / sizeof(arr[0]);
+	for(int i = 0 ; i < size ; i++)
+	{
+		hash[arr[i]] = i ;	//marking seen members
+	}
 	for(int i = 0 ; i < size ; i++){
-		hash[arr[i]] = i ;			//marking seen members
+					
 		if(arr[i] < sum){
-			if(hash[sum - arr[i]] != i){ 	//check if complement of number is seen or not , if seen it should not be equal to 1
+			if(hash[sum - arr[i]] != 0){ 	//check if complement of number is seen or not , if seen it should not be equal to i
 				printf("Pairs : %d and %d", sum-arr[i] , arr[i]); 
 				break;
 			}

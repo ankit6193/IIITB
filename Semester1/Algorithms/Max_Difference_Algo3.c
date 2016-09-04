9e88b5404b93f2ea7cd1d9cd10ad0c8505bfd20d
ankit6193 <ankit.naik93@gmail.com>
2016-09-04 22:41:06 +0530

    Max Difference with condition i <= j <= i+l

---
+++ Semester1/Algorithms/Max_Difference_Algo3.c
@@ -0,0 +1,38 @@
+/* Maximum absolute difference with i<=j<=i+l   --> Keeping track of two elements --> Max difference and minimum element found so far --> O(n) */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int maxDifference(int arr[] , int size , int l){
+	int max = abs(arr[1] - arr[0]) ;
+	int min = arr[0] ;
+	int minIndex = 0;
+	int maxIndex = 1;
+	for(int i = 1 ; i < size ; i++){
+	
+		if(abs(arr[i] - min) > max){
+			maxIndex = i;
+			if((maxIndex - minIndex) <= l && minIndex <= maxIndex){
+				printf("%d max\n",i);
+				max = arr[i] - min ;
+				
+			}
+		}
+
+		if(arr[i] < min){
+			printf("%d min\n",i);
+			min = arr[i] ;
+			minIndex = i ;
+		}
+		
+	}
+	return max ;
+}
+
+int main(){
+	
+	int arr[] = { 2  , 3 , 10 , 15 , 4 , 8 , 15 , 35 , 12 , 55 , 203 , 223 , 450 , 550 , 100, 600} ;
+	int size = sizeof(arr) / sizeof(arr[0]);
+	int l = 5;
+	printf("%d" , maxDifference(arr , size , l)) ;
+}

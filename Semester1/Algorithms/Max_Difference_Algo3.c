2feafc262ddaf4a9a6b253f11bbbf057968a5738
ankit6193 <ankit.naik93@gmail.com>
2016-09-08 01:33:53 +0530

    Max Difference with Sliding Window using Queue implemented using Linked list O(n)

--- Semester1/Algorithms/Max_Difference_Algo3.c
+++ Semester1/Algorithms/Max_Difference_Algo3.c
@@ -1,38 +1,140 @@
-/* Maximum absolute difference with i<=j<=i+l   --> Keeping track of two elements --> Max difference and minimum element found so far --> O(n) */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
-int maxDifference(int arr[] , int size , int l){
-	int max = abs(arr[1] - arr[0]) ;
-	int min = arr[0] ;
-	int minIndex = 0;
-	int maxIndex = 1;
-	for(int i = 1 ; i < size ; i++){
-	
-		if(abs(arr[i] - min) > max){
-			maxIndex = i;
-			if((maxIndex - minIndex) <= l && minIndex <= maxIndex){
-				printf("%d max\n",i);
-				max = arr[i] - min ;
-				
-			}
-		}
+#define MAX( a, b ) ( ( a > b) ? a : b )
 
-		if(arr[i] < min){
-			printf("%d min\n",i);
-			min = arr[i] ;
-			minIndex = i ;
-		}
+
+int minSlidingWindow(int arr[] , int n , int window );
+
+typedef struct node{
+		int data;
+		struct node *next;   //self referential structure
 		
-	}
-	return max;
+} Node;
+
+// ADDING in first position
+
+void pushAtFirst(Node** head , int new_data , size_t data_size){ 	//size_t --> size of any object retured by sizeof operator --> to force no negative number i.e. unsigned int
+
+	Node* new_node = (Node*)malloc(sizeof(Node));
+
+	//new_node -> data = malloc(data_size);
+	new_node -> next = (*head);
+
+	new_node -> data = new_data ;
+
+	(*head) = new_node ;
+
 }
 
-int main(){
+void pushAtLast(Node **node , int new_data , size_t data_size ,Node **last){
+
 	
-	int arr[] = { 2  , 3 , 10 , 15 , 4 , 8 , 15 , 35 , 12 , 55 , 203 , 223 , 450 , 550 , 100, 600} ;
+	Node* new_node = (Node*)malloc(sizeof(Node));
+
+	//new_node -> data = malloc(data_size);
+	new_node -> next = NULL;
+
+	if((*node) == NULL){
+		pushAtFirst(node , new_data , data_size);
+		(*last) = (*node);
+	}else{
+		//printf("This %c\n", *(char *)(*last) -> data );
+		(*last) -> next = new_node;
+		new_node -> data = new_data;
+		(*last) = new_node;
+
+		//printf("This %c\n", *(char *)(*last) -> data );
+	}
+	
+}
+
+void deleteFirst(Node** head){
+	if((*head) == NULL)
+		return;
+	else
+		(*head) = (*head) -> next;
+}
+
+void deleteLast(Node** head , Node** last){
+	if((*head) == NULL){
+		printf("cannot Delete");
+	}else if((*head) -> next == NULL){
+		(*head) = NULL;
+	
+	}
+
+	Node* cur = (*head);
+
+	while(cur -> next -> next != NULL){
+		cur = cur -> next;
+	}
+	
+	(*last) = cur;
+
+	cur -> next = NULL;
+
+}
+	
+	
+
+void printList(Node* node){
+	while(node != NULL){
+		printf("%d\n",node -> data );
+		node = node -> next;
+	}
+}
+
+
+int getLast(Node* last){
+	return last -> data ;
+}
+
+int getFront(Node* head){
+	return head -> data ;
+}
+int main(int argc, char const *argv[])
+{
+	int arr[] = { 1  , 55 , 203 , 3 , 405 , 550 , 100 , 600} ;
 	int size = sizeof(arr) / sizeof(arr[0]);
-	int l = 5;
-	printf("%d" , maxDifference(arr , size , l)) ;
+	int window = 5;
+	printf("%d",minSlidingWindow(arr , size , window));
+
+	return 0;
+}
+
+int minSlidingWindow(int arr[] , int n , int window ){
+	Node *head = NULL ;
+	Node *last = NULL ;
+
+	unsigned int_size = sizeof(int);
+	head = NULL;
+	
+	int c_max = INT_MIN;
+
+	for (int i = 0; i < window ; i++){
+		while(head != NULL && arr[i] < arr[getLast(last)]){
+			deleteLast(&head , &last);
+		}
+		pushAtLast(&head , i , int_size , &last);
+		c_max = MAX(c_max , arr[i] - arr[getFront(head)]);
+		//printf("%d\n",*(int *) (head) -> data );
+	}
+	//printList(head);
+
+	for (int i = window; i < n; ++i)
+	{
+	    while(head != NULL && arr[i] < arr[getLast(last)]){
+	    	deleteLast(&head , &last);
+	    }
+	    if(head != NULL && getFront(head) <= i - window)
+	    	deleteFirst(&head);
+
+	    pushAtLast(&head , i , int_size , &last);
+	    c_max = MAX(c_max , arr[i] - arr[getFront(head)]);
+	}
+
+	//printList(head);
+	return c_max;
 }
